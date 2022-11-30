fea3ea922c66a90c8dc96ca9d0b7a132f08bffcf
Sai Teja <saitejgainer@gmail.com>
2022-11-30 15:03:53 +0530

    Deleted 2 files

--- app/src/main/java/com/tummoc/travel/mvvm/DetailViewModel.kt
+++ app/src/main/java/com/tummoc/travel/mvvm/DetailViewModel.kt
@@ -1,10 +1,6 @@
 package com.tummoc.travel.mvvm
 
-import android.view.View
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import com.tummoc.travel.fragments.DetailFragment
-import kotlinx.coroutines.launch
 
 class DetailViewModel : ViewModel() {
 
